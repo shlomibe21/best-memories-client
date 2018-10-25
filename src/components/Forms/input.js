51f86da7cc5776e916decc0bb6201f87ca3b1ad3
Shlomi <shlomi@onca-inc.com>
2018-10-25 14:28:08 -0700

    I'm trying to solve a deployment issue on TravisCi, it is complaining about the input file. Changed it to see if it helps.

--- src/components/Forms/input.js
+++ src/components/Forms/input.js
@@ -17,51 +17,21 @@ export default class Input extends React.Component {
     if (this.props.meta.touched && this.props.meta.warning) {
       warning = <div className="form-warning">{this.props.meta.warning}</div>;
     }
-    let input;
-    if (this.props.type === "textarea") {
-      input = (
-        <textarea
-          {...this.props.input}
-          {...this.props}
-          id={this.props.input.name}
-          type={this.props.type}
-          className="form-input-textarea"
-          ref={input => (this.input = input)}
-        />
-      );
-    } else if (this.props.type === "date") {
-      input = (
-        <input
-          {...this.props.input}
-          {...this.props}
-          id={this.props.input.name}
-          type={this.props.type}
-          className="form-input disabled-date"
-          ref={input => (this.input = input)}
-        />
-      );
-    } else {
-      input = (
-        <input
-          {...this.props.input}
-          {...this.props}
-          id={this.props.input.name}
-          type={this.props.type}
-          className="form-input"
-          ref={input => (this.input = input)}
-        />
-      );
-    }
+
     return (
-      <div className="form-input-field">
-        <div>
-          <label htmlFor={this.props.input.name}>
-            {this.props.label}
-            {error}
-            {warning}
-          </label>
-        </div>
-        {input}
+      <div className="form-input">
+        <label htmlFor={this.props.input.name} className="signup-login-label">
+          {this.props.label}
+          {error}
+          {warning}
+        </label>
+        <input
+          {...this.props.input}
+          id={this.props.input.name}
+          type={this.props.type}
+          ref={input => (this.input = input)}
+          className="signup-login-input"
+        />
       </div>
     );
   }
