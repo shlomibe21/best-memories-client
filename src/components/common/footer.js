d2b8df3a777a7e7799a488945613cc2e36dacbfe
Shlomi Benshlomo <shlomibe@hotmail.com>
2018-12-05 14:09:58 -0800

    Changed text in footer to automatically advance the year.

--- src/components/common/footer.js
+++ src/components/common/footer.js
@@ -3,9 +3,12 @@ import React from "react";
 import "./footer.css";
 
 export default function Footer(props) {
+  let today = new Date();
+  let year = today.getFullYear();
+  let footerText = `© ${year}, Shlomi Benshlomo (Onca, Inc.)`
   return (
     <footer>
-      <p>© 2018 Onca, Inc. Shlomi Benshlomo</p>
+      <p>{footerText}</p>
     </footer>
   );
 }
